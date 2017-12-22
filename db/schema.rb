9d237e902bec1f87a74e6ace5e2129b4304a9608
tabea89 <t-simon@hotmail.de>
2017-12-22 16:14:08 +0100

    changed price datatype to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171222143659) do
+ActiveRecord::Schema.define(version: 20171222150506) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171222143659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.string "price"
+    t.integer "price"
   end
 
 end
