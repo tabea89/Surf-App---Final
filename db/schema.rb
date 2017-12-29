c7b1f7286fe553d0ea19a16bbb2f48eb4bf489ab
tabea89 <t-simon@hotmail.de>
2017-12-29 19:46:06 +0100

    5.2 Search form and styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225180103) do
+ActiveRecord::Schema.define(version: 20171229113934) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
