f237d38173ae7fe3093c8e2244241f7eb658f315
tabea89 <t-simon@hotmail.de>
2017-12-29 12:26:15 +0100

    5.1 Added routes and added styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  get 'simple_pages/about'
+  get '/about', to: 'simple_pages#about'
 
-  get 'simple_pages/contact'
+  get '/contact', to: 'simple_pages#contact'
 
-  get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  get '/home', to: 'simple_pages#landing_page'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
