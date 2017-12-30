6126fcbce5f82a88fdef463fba0e3a1675bb132a
tabea89 <t-simon@hotmail.de>
2017-12-30 14:57:14 +0100

    5.3 Added contact form + functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'simple_pages#contact'
 
+  post 'simple_pages/thank_you'
+
   get '/home', to: 'simple_pages#landing_page'
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
