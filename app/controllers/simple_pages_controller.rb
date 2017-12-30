45aa3335063f37e96e47c56becf160fec8eb4525
tabea89 <t-simon@hotmail.de>
2017-12-30 14:57:14 +0100

    5.3 Added contact form + functionality

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -6,6 +6,16 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(4)
   end
 
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 't-simon@hotmail.de',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+end
+
 end
 
 
