c39fc8b0fc6dcc6151c966138ea4520dea7b3736
tabea89 <t-simon@hotmail.de>
2018-01-01 14:36:58 +0100

    5.3 updated mailer and controller files

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -6,12 +6,14 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(4)
   end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
+ def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
+
+
 
 end
 
