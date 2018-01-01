3ca1a4f51f493b2da43ea329b2f293e5d08fdc11
tabea89 <t-simon@hotmail.de>
2018-01-01 13:58:11 +0100

    Deleted faulty migration

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,4 @@ class ApplicationMailer < ActionMailer::Base
   default from: 't-simon@hotmail.de'
   layout 'mailer'
 
-  ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
 end
