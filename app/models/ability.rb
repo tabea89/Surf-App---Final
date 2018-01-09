69c0c37333524a9df58d3ceff315fa8550778f44
tabea89 <t-simon@hotmail.de>
2018-01-09 20:42:23 +0100

    5.10 Moderating comments + admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    cannot :destroy, Comment
+    if user.admin?  # additional permissions for administrators
+        can :manage, :all
   end
 
 end
