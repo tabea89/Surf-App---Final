a8943a5c5145f62a20736827273def547e362342
tabea89 <t-simon@hotmail.de>
2018-01-09 21:12:26 +0100

    Changed comments per page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,7 +70,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 1)
+    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
   private
