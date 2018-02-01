81baf88e2b183418cd9a5e6acb33626dbbda41a6
tabea89 <t-simon@hotmail.de>
2018-02-01 19:50:52 +0100

    Redis 2.0

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,12 +12,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # GET /products/1
-  # GET /products/1.json
-  def show
-    @product.viewed!
-  end
-
   # GET /products/new
   def new
     @product = Product.new
@@ -70,6 +64,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    @product.viewed!
     @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
